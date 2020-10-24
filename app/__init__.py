c9a41192997910d835c30b97f44a5e8a07e928d1
PlusMath <fbf78900@gmail.com>
2020-10-25 03:33:41 +0900

    자동 시간 추가

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
- 
+import time
+import threading 
 app = Flask(__name__)
-
 from app.main.index import main as main
+def inmo():
+    
+    
+    app.register_blueprint(main)
 
-app.register_blueprint(main)
+    threading.Timer(60, inmo).start()
+
+inmo()
