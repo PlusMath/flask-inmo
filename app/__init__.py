1ef2714b82d12675d6b5ab7c334917c9dcea6e78
PlusMath <fbf78900@gmail.com>
2020-11-05 00:19:55 +0900

    create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
 
 
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    from app.main.index import main as main
 
-from app.main.index import main as main
-
-app.register_blueprint(main)
+    app.register_blueprint(main)
  
     
 
-app.run(host="0.0.0.0", port=80)
+    return app
 
 
 
