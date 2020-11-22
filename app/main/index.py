c098ff8ac5ec6fe1f4d5af04c8d3190f7aba343b
PlusMath <fbf78900@gmail.com>
2020-11-22 23:36:06 +0900

    one more time

--- app/main/index.py
+++ app/main/index.py
@@ -84,7 +84,30 @@ for item in data:
 #     final_2.insert(i,item_1)
 #     i = i + 1
 
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('disable-gpu')
+chrome_options.add_argument('disable-infobars')
+chrome_options.add_argument('--disable-extensions') 
+prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}
+chrome_options.add_experimental_option('prefs', prefs)
+chrome_options.add_argument("start-maximized") 
+chrome_options.add_argument('--disable-dev-shm-usage') 
 
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://trends.google.co.kr/trends/?geo=KR")
+req = driver.page_source
+soup = BeautifulSoup(req,'html.parser')
+
+data = soup.select('div.list-item-title')
+
+final_2 = []
+i = 0
+for item in data:
+    item_1 = item.get_text()
+    final_2.insert(i,item_1)
+    i = i + 1
 
 
 
@@ -187,16 +210,16 @@ def inmo_1():
     zumdata9 = final_1[16]
     zumdata10 = final_1[18]
 
-    # googledata1 = final_2[0]
-    # googledata2 = final_2[1]
-    # googledata3 = final_2[2]
-    # googledata4 = final_2[3]
-    # googledata5 = final_2[4]
-    # googledata6 = final_2[5]
-    # googledata7 = final_2[6]
-    # googledata8 = final_2[7]
-    # googledata9 = final_2[8]
-    # googledata10 = final_2[9]
+    googledata1 = final_2[0]
+    googledata2 = final_2[1]
+    googledata3 = final_2[2]
+    googledata4 = final_2[3]
+    googledata5 = final_2[4]
+    googledata6 = final_2[5]
+    googledata7 = final_2[6]
+    googledata8 = final_2[7]
+    googledata9 = final_2[8]
+    googledata10 = final_2[9]
 
     melondata1 = final_3[0]
     melondata2 = final_3[1]
@@ -237,7 +260,7 @@ def inmo_1():
 
     return render_template('/inmo/table.html', naverdata_1=naverdata1,naverdata_2=naverdata2,naverdata_3=naverdata3,naverdata_4=naverdata4,naverdata_5=naverdata5,naverdata_6=naverdata6,naverdata_7=naverdata7,naverdata_8=naverdata8,naverdata_9=naverdata9,naverdata_10=naverdata10,
     zumdata_1=zumdata1,zumdata_2=zumdata2,zumdata_3=zumdata3,zumdata_4=zumdata4,zumdata_5=zumdata5,zumdata_6=zumdata6,zumdata_7=zumdata7,zumdata_8=zumdata8,zumdata_9=zumdata9,zumdata_10=zumdata10,
-    # googledata_1=googledata1,googledata_2=googledata2,googledata_3=googledata3,googledata_4=googledata4,googledata_5=googledata5,googledata_6=googledata6,googledata_7=googledata7,googledata_8=googledata8,googledata_9=googledata9,googledata_10=googledata10,
+    googledata_1=googledata1,googledata_2=googledata2,googledata_3=googledata3,googledata_4=googledata4,googledata_5=googledata5,googledata_6=googledata6,googledata_7=googledata7,googledata_8=googledata8,googledata_9=googledata9,googledata_10=googledata10,
     melondata_1=melondata1, melondata_2=melondata2, melondata_3=melondata3, melondata_4=melondata4, melondata_5=melondata5, melondata_6=melondata6, melondata_7=melondata7, melondata_8=melondata8, melondata_9=melondata9, melondata_10=melondata10,
     geniedata_1=geniedata1, geniedata_2=geniedata2, geniedata_3=geniedata3, geniedata_4=geniedata4, geniedata_5=geniedata5, geniedata_6=geniedata6, geniedata_7=geniedata7, geniedata_8=geniedata8, geniedata_9=geniedata9, geniedata_10=geniedata10,
     bugsdata_1=bugsdata1, bugsdata_2=bugsdata2, bugsdata_3=bugsdata3, bugsdata_4=bugsdata4, bugsdata_5=bugsdata5, bugsdata_6=bugsdata6, bugsdata_7=bugsdata7, bugsdata_8=bugsdata8, bugsdata_9=bugsdata9, bugsdata_10=bugsdata10
